53e472a4e4c3126c165cbc8c8d31ac88a427bf77
Bavithran B <thran8954@gmail.com>
2025-07-16 13:30:29 +0530

    Update input.py

--- input.py
+++ input.py
@@ -1,4 +1,13 @@
 # Taking input from the user
+
+"""
+Input and Output (I/O) refers to the communication between a program and the outside world:
+
+Input: Data received by the program (from the user, file, etc.).
+Output: Data sent from the program (to the screen, file, etc.).
+Python provides built-in functions and file handling capabilities for both.
+"""
+
 name = input("Enter your name: ")
 age = input("Enter your age: ")
 
